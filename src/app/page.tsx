1c78b68537fc6079df566e69db74316c451cd015
garala <StevenGarcia.cs@gmail.com>
2025-10-22 00:31:22 -0500

    quick changes to html/css. nothing major.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import css from "./page.module.css";
+
 export default function Home() {
-  return <div>blur blur blur</div>;
+  return (
+    <main className={css.pageContainer}>
+      <section className={css.mainSection}></section>
+    </main>
+  );
 }
